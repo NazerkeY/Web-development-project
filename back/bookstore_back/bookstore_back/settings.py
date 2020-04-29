a043679cda13d3cd8ed639a7991ca36f4e77a059
Mubarak Tursynbekov <kuanyshbekuly1002@gmail.com>
2020-04-29 14:17:20 +0600

    db images

--- back/bookstore_back/bookstore_back/settings.py
+++ back/bookstore_back/bookstore_back/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework_jwt',
     'corsheaders',
+    'django_filters'    ,
 
     'api',
 ]
@@ -56,6 +57,8 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ORIGIN_ALLOW_ALL = True
 
 ROOT_URLCONF = 'bookstore_back.urls'
 
