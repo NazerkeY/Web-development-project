7e63b4f2de4b6f32b2d6c04e60ae29ccda130e16
Nazerke Yeginay <nazerke_5@inbox.ru>
2020-04-30 16:08:55 +0300

    authors and categories from back

--- front/src/app/product-list/product-list.component.ts
+++ front/src/app/product-list/product-list.component.ts
@@ -2,7 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { BookshopService} from '../service/bookshop.service';
 import { ActivatedRoute} from '@angular/router';
 import { Product } from '../database/product';
-import {Book} from '../books';
+//import {Book} from '../books';
+import {Book} from '../models';
+
 
 @Component({
   selector: 'app-product-list',
@@ -30,5 +32,20 @@ export class ProductListComponent implements OnInit {
   .subscribe(products => this.products = products);
   }
 
+/*products: Book[]
+constructor(private bookshopService: BookshopService,
+public route:ActivatedRoute){}
+
+ngOnInit(): void{
+this.getProducts();
+}
+
+getProducts(){
+let id = this.route.snapshot.paramMap.get('id');
+id = id.substr(2);
+this.bookshopService.getProducts(id).subscribe(products => {
+this.products = products;
+});
+} */
 
 }
